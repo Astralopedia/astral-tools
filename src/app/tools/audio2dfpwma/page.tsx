cfd4fa22de267f25342db9b236def5cd55c84c51
Nguyen Minh Tho <definedentity@proton.me>
2024-08-13 22:55:52 +0700

    It works

---
+++ src/app/tools/audio2dfpwma/page.tsx
@@ -0,0 +1,3 @@
+export default function Audio2DFPWMA() {
+	return <div>Audio2DFPWMA</div>
+}
--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -1,10 +1,19 @@
 import ToolContainer from "@/components/ToolContainer"
+import { ToolItemType } from "@/components/ToolItem"
 
 function Tools() {
+	const tools: ToolItemType[] = [
+		{
+			name: "Audio to DFPWMA converter",
+			description: "Convert audio to DFPWMA",
+			href: "/tools/audio2dfpwma",
+		},
+	]
+
 	return (
 		<div>
-			<p className="text-center text-3xl my-6">Available Tools</p>
-			<ToolContainer />
+			<p className='text-center text-3xl my-6'>Available Tools</p>
+			<ToolContainer items={tools} />
 		</div>
 	)
 }
